d52c5ec9923841c7f90030ea6f82f8d02c68fdae
Giovanna <giovanna.r@aluno.ifsc.edu.br>
2026-03-24 20:12:48 -0300

    Inicio Exercicio 2 - Horario

---
+++ lab-03-ex2/src/main/java/ads/poo/Horário.java
@@ -0,0 +1,28 @@
+package ads.poo;
+
+public class Horário {
+
+    //atributos
+    private int hora;
+    private int minuto;
+    private int segundo;
+
+
+    //métodos
+
+
+    public Horário() {
+        this.hora = 0;
+        this.minuto = 0;
+        this.segundo = 0;
+    }
+
+
+
+    public boolean setHora (int h) {
+        if (h>=0 && h<24)
+    }
+
+
+
+}
---
+++ lab-03-ex2/src/main/java/ads/poo/Main.java
@@ -0,0 +1,12 @@
+package ads.poo;
+
+
+public class Main {
+    static void main(String[] args) {
+
+
+
+
+
+    }
+}
